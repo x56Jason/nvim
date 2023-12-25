1dc7774c7dc20ff2df7b52d15948919eb9889a60
Jason Zeng <zrzeng@gmail.com>
2023-12-25 13:14:58 +0800

    autocmds: highlight on yank

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,6 +2,14 @@ local function augroup(name)
   return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup("last_loc"),
