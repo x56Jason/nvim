ed03bb3a17c86939c2151363dd267ba9772cb7aa
Jason Zeng <zrzeng@gmail.com>
2023-12-30 21:31:42 +0800

    treesitter: make it lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,6 +58,7 @@ return {
 
 	{
 		"nvim-treesitter/nvim-treesitter",
+		event = { "VeryLazy", "BufReadPost", "BufNewFile", "BufWritePre" },
 		build = ":TSUpdate",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
