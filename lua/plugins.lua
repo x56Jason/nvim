e3e34d454bba23e83a6b487afb91aaa2bb0b6257
Jason Zeng <zrzeng@gmail.com>
2023-12-30 10:06:34 +0800

    telescope: make it verylazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,6 +28,7 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+		event = "VeryLazy",
 		config = function() require("config.telescope") end,
 	},
 
