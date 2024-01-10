26b033db289c70c54641338c2f447f2bb2f47a31
Jason Zeng <zrzeng@gmail.com>
2024-01-10 16:21:43 +0800

    lsp: lua-language-server refactor

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -61,32 +61,64 @@ nvim_lsp.pylsp.setup {
 	filetypes = { "python" }
 }
 
-local lua_ls_root_path = os.getenv("HOME") .. "/lua-language-server"
-local lua_ls_binary = lua_ls_root_path .. "/bin/lua-language-server"
-
 nvim_lsp.lua_ls.setup {
-	cmd = { lua_ls_binary, "-E", lua_ls_root_path .. "/main.lua"},
-	settings = {
-		Lua =  {
-			runtime = {
-				version = 'LuaJIT',
-				path = vim.split(package.path, ';')
-			},
-			completion = {enable = true, callSnippet = "Both"},
-			diagnostics = {
-				enable = true,
-				globals = { 'vim', 'describe' },
-				disable = { "lowercase-global" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
-					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-				},
-			},
-		},
-	},
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              vim.fn.stdpath("data") .. "/lazy/",
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
 }
+
+--local lua_ls_root_path = os.getenv("HOME") .. "/lua-language-server"
+--local lua_ls_binary = lua_ls_root_path .. "/bin/lua-language-server"
+
+--nvim_lsp.lua_ls.setup {
+--	cmd = { lua_ls_binary, "-E", lua_ls_root_path .. "/main.lua"},
+--	settings = {
+--		Lua =  {
+--			runtime = {
+--				version = 'LuaJIT',
+--				path = vim.split(package.path, ';')
+--			},
+--			completion = {enable = true, callSnippet = "Both"},
+--			diagnostics = {
+--				enable = true,
+--				globals = { 'vim', 'describe' },
+--				disable = { "lowercase-global" },
+--			},
+--			workspace = {
+--				library = {
+--					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+--					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+--				},
+--			},
+--		},
+--	},
+--}
 
 -- Diagnostics
 vim.diagnostic.config({
