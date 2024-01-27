99678a847e3ca75bab2f34704d008ce7040c9863
Jason Zeng <zrzeng@gmail.com>
2024-01-27 13:59:19 +0800

    notify adding harpoon mark

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -1,8 +1,14 @@
+local mark = require("harpoon.mark")
+function my_harpoon_add_file()
+	mark.add_file()
+	local f = vim.api.nvim_buf_get_name(0)
+	vim.notify("Harpoon: <" .. f .. "> added", vim.log.levels.INFO, {})
+end
 
 local map = vim.api.nvim_set_keymap
 local default_options = {noremap = true, silent = true}
 
-map("n", "m", "<cmd>lua require('harpoon.mark').add_file()<CR>", default_options)
+map("n", "m", "<cmd>lua my_harpoon_add_file()<CR>", default_options)
 map("n", "MM", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", default_options)
 map("n", "M1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", default_options)
 map("n", "M2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", default_options)
