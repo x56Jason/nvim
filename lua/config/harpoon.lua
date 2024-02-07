d0a58849a650ae82fc6d199fa2fd32b6b46c833e
Jason Zeng <zrzeng@gmail.com>
2024-02-07 11:49:36 +0800

    toggle buffer if harpoon list length equals to 0

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -14,6 +14,22 @@ local toggle_opts = {
 	title_pos = "center",
 }
 
+local function toggle_next_buffer()
+	if harpoon:list():length() == 0 then
+		vim.cmd("bnext")
+	else
+		harpoon:list():next({ui_nav_wrap = true})
+	end
+end
+
+local function toggle_prev_buffer()
+	if harpoon:list():length() == 0 then
+		vim.cmd("bprevious")
+	else
+		harpoon:list():prev({ui_nav_wrap = true})
+	end
+end
+
 vim.keymap.set("n", "m", function() my_harpoon_add_file() end)
 vim.keymap.set("n", "MM", function() harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts) end)
 vim.keymap.set("n", "M1", function() harpoon:list():select(1) end)
@@ -23,5 +39,5 @@ vim.keymap.set("n", "M4", function() harpoon:list():select(4) end)
 vim.keymap.set("n", "M5", function() harpoon:list():select(5) end)
 
 -- Tab switch buffer
-vim.keymap.set("n", "<TAB>", function() harpoon:list():next() end)
-vim.keymap.set("n", "<S-TAB>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<TAB>", function() toggle_next_buffer() end)
+vim.keymap.set("n", "<S-TAB>", function() toggle_prev_buffer() end)
